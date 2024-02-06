b93f47d8c831771a98c30554175bb9e6337222f1
Kilian <kiliantravail@gmail.com>
2024-02-07 00:35:27 +0100

    POST request works

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiController extends AbstractController
+{
+
+    #[Route('/api/adresses', methods:['POST'])]
+    public function searchAddress(Request $request) : JsonResponse
+    {
+        $postData = json_decode($request->getContent(), true);
+    
+        // Vérifier si le champ "adresse" existe dans les données envoyées
+        if (!isset($postData["adresse"])) {
+            return new JsonResponse(['message' => 'Le champ "adresse" est requis'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $address = $postData["adresse"];
+        $url = 'https://api-adresse.data.gouv.fr/search/?q=' . urlencode($address) . '&autocomplete=1';
+        dump($url);
+
+        try {
+            $httpClient = HttpClient::create();
+            $response = $httpClient->request('GET', $url);
+            $bodyString = $response->getContent();
+            $bodyArray = json_decode($bodyString, true);
+            $finalResult = [];
+            foreach ($bodyArray['features'] as $feature) {
+                $tmpArray = [
+                    'adresse' => $feature['properties']['label'],
+                    'ville' => $feature['properties']['city']
+                ];
+                $finalResult[] = $tmpArray;
+            }
+
+            return new JsonResponse($finalResult);
+        } catch (\Exception $e) {
+            return 'Erreur lors de la requête à l\'API Adresse : ' . $e->getMessage();
+        }
+    }
+}
